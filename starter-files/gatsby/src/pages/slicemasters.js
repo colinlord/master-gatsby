83528407d9d8e0ae46c8616a7258620e2fa307de
Colin Lord <colin@kt4atl.com>
2021-01-14 10:37:42 -0600

    fix for duplicate nav and footer

--- starter-files/gatsby/src/pages/slicemasters.js
+++ starter-files/gatsby/src/pages/slicemasters.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout';
 
 export default function SlicemastersPage() {
   return (
-    <Layout>
+    <>
       <p>Hey! I'm a slidemaster page!</p>
-    </Layout>
+    </>
   );
 }
